a7cf8fe7d49cb2eb330a9a1133b6f0dbc47f60a1
Adem S. Beyhan <selbeyhan@gmail.com>
2024-10-08 09:26:19 -0500

    putting my vim on git so I can sync between devices

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,26 @@
+local config = function()
+  require("nvim-treesitter.configs").setup({
+    indent = { enable = true, },
+    autotag = { enable = true, },
+    ensure_installed = 
+    {
+      "json",
+      "html",
+      "css",
+      "bash",
+      "lua",
+      "gitignore",
+      "python",
+    },
+    auto_install = true,
+    highlight = { enable = true, 
+    additional_vim_regex_highlighting = true, 
+    },
+  })
+end
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  config = config,
+}
